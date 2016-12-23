94b080b0753ede7e645014e6450bab070b76ef1f
Boyuzhou <386607913@qq.com>
2016-12-23 14:03:42 +0800

    change demo

--- demo/demolist/Demo2.js
+++ demo/demolist/Demo2.js
@@ -9,12 +9,12 @@ class Demo2 extends Component {
     render () {
         return (
             <Row>
-                <Col md={3} mdOffset={3} xs={3} xsOffset={3} sm={3} smOffset={3}><div className='grayDeep'></div></Col>
-                <Col md={3} mdOffset={3} xs={3} xsOffset={3} sm={3} smOffset={3}><div className='gray'></div></Col>
-                <Col md={6} mdOffset={6} xs={6} xsOffset={6} sm={6} smOffset={6}><div className='grayLight'></div></Col>
-                <Col md={4} mdOffset={2} xs={4} xsOffset={2} sm={4} smOffset={2}><div className='gray'></div></Col>
-                <Col md={2} mdOffset={3} xs={2} xsOffset={3} sm={2} smOffset={3}><div className='grayLight'></div></Col>
-                <Col md={6} mdOffset={3} xs={6} xsOffset={3} sm={6} smOffset={3}><div className='grayDeep'></div></Col>
+                <Col md={3} mdOffset={3} xs={3} xsOffset={3} sm={3} smOffset={3}><div className='grayDeep'>3 offset-3</div></Col>
+                <Col md={3} mdOffset={3} xs={3} xsOffset={3} sm={3} smOffset={3}><div className='gray'>3 offset-3</div></Col>
+                <Col md={6} mdOffset={6} xs={6} xsOffset={6} sm={6} smOffset={6}><div className='grayLight'>6 offset-6</div></Col>
+                <Col md={4} mdOffset={2} xs={4} xsOffset={2} sm={4} smOffset={2}><div className='gray'>4 offset-2</div></Col>
+                <Col md={2} mdOffset={3} xs={2} xsOffset={3} sm={2} smOffset={3}><div className='grayLight'>2 offset-3</div></Col>
+                <Col md={6} mdOffset={3} xs={6} xsOffset={3} sm={6} smOffset={3}><div className='grayDeep'>6 offset-3</div></Col>
             </Row>
         )
     }
--- demo/demolist/Demo3.js
+++ demo/demolist/Demo3.js
@@ -11,10 +11,10 @@ class Demo3 extends Component {
         return (
             <Row>
             <Col md={8} mdPush={4} xs={8} xsPush={4} sm={8} smPush={4} >
-            <div className='grayDeep'></div>
+            <div className='grayDeep'>8 push-4</div>
             </Col>
             <Col md={4} mdPull={8} xs={4} xsPull={8} sm={4} smPull={8}  >
-            <div className='gray'></div>
+            <div className='gray'>4 pull-8</div>
             </Col>
             </Row>
         )
