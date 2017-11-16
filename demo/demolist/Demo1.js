85b1d065464c1ea80c0589a19c14ecce1a5df821
BoyuZhou <386607913@qq.com>
2017-11-16 17:34:33 +0800

    dist

--- demo/demolist/Demo1.js
+++ demo/demolist/Demo1.js
@@ -11,7 +11,6 @@ import { Con, Row, Col } from '../../src';
 class Demo1 extends Component {
     render () {
         return (
-        <Con componentClass="div">
             <Row>
                 <Col md={12} xs={12} sm={12} ><div className='grayDeep'>12</div></Col>
                 <Col md={6} xs={6} sm={6} ><div className='gray'>6</div></Col>
@@ -30,7 +29,6 @@ class Demo1 extends Component {
                 <Col md={2}  xs={2} sm={2}><div className='grayLight'>2</div></Col>
                 <Col md={2}  xs={2} sm={2}><div className='grayDeep'>2</div></Col>
             </Row>
-        </Con>
 
         )
     }
