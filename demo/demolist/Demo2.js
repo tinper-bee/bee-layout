706329d772b2e078fcb4eebf457af098154e67a2
Boyuzhou <2470200492@qq.com>
2018-01-21 19:10:36 +0800

    fix: 修改示例

--- demo/demolist/Demo2.js
+++ demo/demolist/Demo2.js
@@ -5,19 +5,31 @@
  *
  */
 
-import React, { Component } from 'react';
-import { Con, Row, Col } from '../../src';
+import React, {Component} from 'react';
+import {Col, Row} from '../../src';
 
 class Demo2 extends Component {
-    render () {
+    render() {
         return (
             <Row>
-                <Col md={3} mdOffset={3} xs={3} xsOffset={3} sm={3} smOffset={3}><div className='grayDeep'>3 offset-3</div></Col>
-                <Col md={3} mdOffset={3} xs={3} xsOffset={3} sm={3} smOffset={3}><div className='gray'>3 offset-3</div></Col>
-                <Col md={6} mdOffset={6} xs={6} xsOffset={6} sm={6} smOffset={6}><div className='grayLight'>6 offset-6</div></Col>
-                <Col md={4} mdOffset={2} xs={4} xsOffset={2} sm={4} smOffset={2}><div className='gray'>4 offset-2</div></Col>
-                <Col md={2} mdOffset={3} xs={2} xsOffset={3} sm={2} smOffset={3}><div className='grayLight'>2 offset-3</div></Col>
-                <Col md={6} mdOffset={3} xs={6} xsOffset={3} sm={6} smOffset={3}><div className='grayDeep'>6 offset-3</div></Col>
+                <Col md={3} mdOffset={3} xs={3} xsOffset={3} sm={3} smOffset={3}>
+                    <div className='grayDeep'>3 offset-3</div>
+                </Col>
+                <Col md={3} mdOffset={3} xs={3} xsOffset={3} sm={3} smOffset={3}>
+                    <div className='gray'>3 offset-3</div>
+                </Col>
+                <Col md={6} mdOffset={6} xs={6} xsOffset={6} sm={6} smOffset={6}>
+                    <div className='grayLight'>6 offset-6</div>
+                </Col>
+                <Col md={4} mdOffset={2} xs={4} xsOffset={2} sm={4} smOffset={2}>
+                    <div className='gray'>4 offset-2</div>
+                </Col>
+                <Col md={2} mdOffset={3} xs={2} xsOffset={3} sm={2} smOffset={3}>
+                    <div className='grayLight'>2 offset-3</div>
+                </Col>
+                <Col md={6} mdOffset={3} xs={6} xsOffset={3} sm={6} smOffset={3}>
+                    <div className='grayDeep'>6 offset-3</div>
+                </Col>
             </Row>
         )
     }
